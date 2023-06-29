e515b3162bd23ee9ab4348f1b59cce993c762441
Sebastian Holt <sholt@ucsd.edu>
2023-06-29 15:47:48 -0400

    cleaning up for CogSci

---
+++ analysis/analyses.R
@@ -0,0 +1,172 @@
+library('tidyverse')
+library('ggplot2')
+library('lme4')
+library(lmerTest)
+library(emmeans)
+library(data.table)
+library(ggplot2)
+
+# First, import the data
+Q1 <- read_csv("../data/Q1.csv")
+Q2 <- read_csv("../data/Q2.csv")
+M2 <- read_csv("../data/M2.csv")
+
+# convert time variables back into milliseconds. Not important, but seems standard
+Q1$RT <- Q1$RT*1000
+Q1$WT <- Q1$WT*1000
+Q1$CT <- Q1$CT*1000
+Q2$RT <- Q2$RT*1000
+Q2$WT <- Q2$WT*1000
+Q2$CT <- Q2$CT*1000
+M2$RT <- M2$RT*1000
+
+# In Experiment 1, we pre-registered two slightly different experiments. Analyze separately:
+Q1a <- Q1 %>% filter(base %in% c(2, 3, 4, 5))
+Q1b <- Q1 %>% filter(base %in% c(4, 8, 10))
+
+# This document contains our pre-registered analyses. Annotations reflect one major difference,
+# which is increased Experiment 1a sample size, requested by reviewers. Also, we will check if
+# our pre-registered time DV (RT + (WT / word_length)) differs from simple reaction time (RT).
+# Finally, it contains an exploratory model probing effects of different rules of composition.
+
+
+## Experiment 1a - Analysis 15.1   #######################
+# maximal models:
+CT_Q1a_max <- lmer( data=Q1a, CT ~ base + ( 1 + num | GID )) # NO CONVERGE
+AC_Q1a_max <- glmer(data=Q1a, correct ~ base + ( 1 + num | GID ), family='binomial') # SINGULAR
+
+# final models:
+CT_Q1a <- lmer( data=Q1a, CT ~ base + ( 1 | GID ))
+RT_Q1a <- lmer( data=Q1a, RT ~ base + ( 1 | GID )) # similar to CT
+AC_Q1a <- glmer(data=Q1a, correct ~ base + ( 1 | GID ), family='binomial')
+
+sink(file = "../results/models/expt1a.txt")
+summary(CT_Q1a)
+message("#########################################\n next \n#########################################")
+summary(AC_Q1a)
+sink(file = NULL)
+#######################
+
+
+## Experiment 1b - Analysis 15.1   #######################
+# maximal models:
+CT_Q1b_max <- lmer( data=Q1b, CT ~ base + ( 1 + num | GID )) # NO CONVERGE
+AC_Q1b_max <- glmer(data=Q1b, correct ~ base + ( 1 + num | GID ), family='binomial') # converges
+
+# final models (identical):
+CT_Q1b <- lmer( data=Q1b, CT ~ base + ( 1 | GID ))
+RT_Q1b <- lmer( data=Q1b, RT ~ base + ( 1 | GID )) # similar to CT
+AC_Q1b <- glmer(data=Q1b, correct ~ base + ( 1 | GID ), family='binomial') # remove random slope for consistency
+
+sink(file = "../results/models/expt1b.txt")
+summary(CT_Q1b)
+message("#########################################\n next \n#########################################")
+summary(AC_Q1b)
+sink(file = NULL)
+#######################
+
+
+## Experiment 1 - Analysis 15.1   #######################
+# maximal models:
+CT_Q1_max <- lmer( data=Q1, CT ~ base + ( 1 + num | GID )) # NO CONVERGE
+AC_Q1_max <- glmer(data=Q1, correct ~ base + ( 1 + num | GID ), family='binomial') # NO CONVERGE
+
+# final models:
+CT_Q1 <- lmer( data=Q1, CT ~ base + ( 1 | GID ))
+RT_Q1 <- lmer( data=Q1, RT ~ base + ( 1 | GID )) # similar to CT
+AC_Q1 <- glmer(data=Q1, correct ~ base + ( 1 | GID ), family='binomial')
+
+sink(file = "../results/models/expt1.txt")
+summary(CT_Q1)
+message("#########################################\n next \n#########################################")
+summary(AC_Q1)
+sink(file = NULL)
+#######################
+
+
+## Experiment 1 - Exploratory (Rules) #######################
+# the full models both had 'number' as a random slope, but rulesCT didn't converge, so it was removed. Output was qualitatively similar. For rulesAC, output *did* change when the random slope was removed, which seems to suggest it should be left in (it converges)
+rulesCT <- lmer(data=Q1,  CT ~      base + add + mult + exp + numSyls + (1 | GID))
+rulesAC <- glmer(data=Q1, correct ~ base + add + mult + exp + numSyls + (1 + num | GID), family='binomial')
+
+sink(file = "../results/models/expt1_rules.txt")
+summary(rulesCT)
+message("#########################################\n next \n#########################################")
+summary(rulesAC)
+sink(file = NULL)
+#######################
+
+
+
+
+## Experiment 2 - Analysis 15.1   #######################
+# maximal models:
+CT_Q2_max <- lmer( data=Q2, CT      ~ cond * num * freq + (1 | GID)) # converges
+AC_Q2_max <- glmer(data=Q2, correct ~ cond * num * freq + (1 | GID), family='binomial') # NO CONVERGE
+
+# final models:
+CT_Q2 <-      lmer( data=Q2, CT     ~ cond * freq + num + (1 | GID))
+RT_Q2 <-      lmer( data=Q2, RT     ~ cond * freq + num + (1 | GID)) # only freq now non-sig
+AC_Q2 <-     glmer(data=Q2, correct ~ cond * freq + num + (1 | GID), family='binomial')
+
+sink(file = "../results/models/expt2_15.1.txt")
+summary(CT_Q2)
+message("#########################################\n next \n#########################################")
+summary(AC_Q2)
+sink(file = NULL)
+#######################
+
+
+## Experiment 2 - Analysis 15.2   #######################
+# maximal models:
+RT_M2a_max <-  lmer(data=M2, RT ~      cond * numsum * freq + (1 | GID)) # converges
+AC_M2a_max <- glmer(data=M2, correct ~ cond * numsum * freq + (1 | GID), family='binomial') # NO CONVERGE
+
+# final models:
+RT_M2a <-  lmer(data=M2, RT ~      cond * numsum * freq + (1 | GID))
+AC_M2a <- glmer(data=M2, correct ~ cond * numsum + freq + (1 | GID), family='binomial')
+
+sink(file = "../results/models/expt2_15.2.txt")
+summary(RT_M2a)
+message("#########################################\n next \n#########################################")
+summary(AC_M2a)
+sink(file = NULL)
+#######################
+
+
+## Experiment 2 - Analysis 15.2.1 #######################
+# maximal (unaugmented) models:
+RT_M2b_max <-  lmer(data=M2, RT ~      cond * numsum * numdif + (1 | GID)) # converges
+AC_M2b_max <- glmer(data=M2, correct ~ cond * numsum * numdif + (1 | GID), family='binomial') # NO CONVERGE
+
+# final (unaugmented) models (to compare):
+RT_M2b <-  lmer(data=M2, RT ~      cond + numsum + numdif + (1 | GID)) # same as glmer, for consistency
+AC_M2b <- glmer(data=M2, correct ~ cond + numsum + numdif + (1 | GID), family='binomial')
+
+# word-length models (based on final unaugmented models; to compare):
+RT_M2b_wl <-  lmer(data=M2, RT ~      syllDiff * (cond + numdif) + numsum + (1 | GID))
+AC_M2b_wl <- glmer(data=M2, correct ~ syllDiff * (cond + numdif) + numsum + (1 | GID), family='binomial')
+
+# final model comparisons:
+compRT <- anova(RT_M2b,RT_M2b_wl)
+compAC <- anova(AC_M2b,AC_M2b_wl)
+
+sink(file = "../results/models/expt2_15.2.1.txt")
+summary(RT_M2b)
+message("#########################################\n next \n#########################################")
+summary(AC_M2b)
+message("#########################################\n next \n#########################################")
+summary(RT_M2b_wl)
+message("#########################################\n next \n#########################################")
+summary(AC_M2b_wl)
+message("#########################################\n next \n#########################################")
+message("#########################################\n\n#########################################")
+compRT
+message("#########################################\n next \n#########################################")
+compAC
+sink(file = NULL)
+#######################
+
+
+
+
---
+++ analysis/power_analysis.R
@@ -0,0 +1,71 @@
+# Following along with this:
+# https://humburg.github.io/Power-Analysis/simr_power_analysis.html
+
+# Another reference:
+# https://cran.r-project.org/web/packages/simr/vignettes/fromscratch.html
+
+# install.packages('simr')
+library(simr)
+simrOptions(progress=FALSE)
+
+# how many of each factor level is there?
+N = 20        # 20
+bases = 3     # 3
+trials = 65   # 65
+
+# set up the number of factors for each column
+base <- c(4, 8, 10)
+subj <- factor(1:(N*bases))
+trial <- factor(1:trials)
+
+# repeat the factor levels
+base_full <- c(rep(base, each=N*trials))
+subj_full <- c(rep(subj, each=trials))
+trial_full <- c(rep(trial, N))
+
+# put them together into dataframe
+covars <- data.frame(base=base_full, subject=subj_full, trial=trial_full)
+
+# First, let's take some data we already have, model it, and extract expected model parameters
+study1 <- read.csv('numsyn1Data.csv')
+syls <- data.frame(apply(study1,2,nchar))$response / 2
+study1$numSyls <- syls
+study1$RT <-  as.numeric(study1$RT)
+study1$writingTime <-  as.numeric(study1$writingTime)
+study1$trialNum <-  as.numeric(study1$trialNum)
+study1$timeDV <- study1$RT + (study1$writingTime / study1$numSyls)
+model1 <- lmer( timeDV ~ base*trialNum + ( 1 | gameID ), data=study1)
+
+# now let's visualize it
+summary(model1)
+
+# FIRST, fill in effects with what we got previously
+## fixed effects: (Intercept), base, trial, base:trial  
+fixed <- c(2356.361, 65.901, 1.978)#, -1.293 )
+## Random intercepts for participants
+rand <- list(145694.89 ) # Std.Dev. is sqrt of this number
+## residual variance
+res <- 1263.4
+
+# SECOND, replace 'base' coef with an anticipated H1 effect
+## fixed effects: (Intercept), base, trial, base:trial  
+fixed <- c(2356.361, 45, 1.978)#, -1.293 )
+## Random intercepts for participants
+rand <- list(145694.89 ) # Std.Dev. is sqrt of this number
+## residual variance
+res <- 1263.4
+
+# now we make a new model with the expected effects, etc.
+model <- makeLmer(y ~ base+trial + (1|subject),
+                  fixef=fixed, VarCorr=rand, sigma=res, data=covars)
+
+# simular data from such a model 100 times
+sim_treat <- powerSim(model, nsim=100, test = fcompare(y~trial))
+
+# what if we had more subjects per condition?
+model_ext <- extend(model, along="subject", n=120) 
+
+# plot the curve of how much power we'd have to detect expected effects, for every sample size
+p_curve_treat <- powerCurve(model_ext, test=fcompare(y~trial), along="subject")
+plot(p_curve_treat)
+
